115b7008d915a6c36b26bbee609afb114ea45638
EmiyaHana <warm310149@gmail.com>
2026-05-01 01:57:35 +0700

    AmmoSystemComplete

--- StillAlive/Assets/Scripts/PlayerController.cs
+++ StillAlive/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("SettingMovement")]
+    [Header("Setting Movement")]
     public float moveSpeed = 5f;
     public Camera cam;
+
+    [Header("Weapon Range")]
+    public GameObject spinningWeaponPrefab;
+    public Transform firePoint;
+
+    [Header("Ammo System")]
+    public int maxAmmo = 10; // กระสุนสูงสุด
+    private int currentAmmo;
+    public TextMeshProUGUI ammoTextUI;
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -13,6 +23,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        currentAmmo = maxAmmo;
+        UpdateAmmoUI();
     }
 
     void Update()
@@ -21,6 +34,11 @@ public class PlayerController : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
     }
 
     void FixedUpdate()
@@ -30,5 +48,35 @@ public class PlayerController : MonoBehaviour
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+    }
+
+    void Shoot()
+    {
+        if (currentAmmo > 0)
+        {
+            Instantiate(spinningWeaponPrefab, firePoint.position, firePoint.rotation);
+            
+            currentAmmo--;
+            UpdateAmmoUI();
+        }
+        else
+        {
+            Debug.Log("Ammo out!");
+        }
+    }
+
+    public void UpdateAmmoUI()
+    {
+        if (ammoTextUI != null)
+        {
+            ammoTextUI.text = "Ammo: " + currentAmmo + " / " + maxAmmo;
+        }
+    }
+
+    public void AddAmmo(int amount)
+    {
+        currentAmmo += amount;
+        if (currentAmmo > maxAmmo) currentAmmo = maxAmmo;
+        UpdateAmmoUI();
     }
 }
