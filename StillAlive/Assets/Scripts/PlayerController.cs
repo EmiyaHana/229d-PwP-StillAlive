51353690891722baf5113c62e0699a05d68551cb
EmiyaHana <warm310149@gmail.com>
2026-05-01 01:35:07 +0700

    PlayerSetUp

---
+++ StillAlive/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("SettingMovement")]
+    public float moveSpeed = 5f;
+    public Camera cam;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
