6cf797dc7015cb451fb44c4aa299c245c4bc5ffc
EmiyaHana <warm310149@gmail.com>
2026-05-01 01:46:52 +0700

    SpinWeaponComplete

---
+++ StillAlive/Assets/Scripts/SpinningWeapon.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class SpinningWeapon : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 10f;
+
+    [Header("RotationalMotion")]
+    public float targetAngularVelocity = 1000f;
+    public float timeToReachSpeed = 0.5f;
+
+    private Rigidbody2D rb;
+    private float momentOfInertia; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.linearVelocity = transform.up * moveSpeed; 
+
+        momentOfInertia = rb.inertia; //Inertia for object
+
+        float angularAcceleration = targetAngularVelocity / timeToReachSpeed; //Calculates for Acceleration
+
+        float appliedTorque = momentOfInertia * angularAcceleration; //calculates for Torque force (Tau = I * Alpha)
+
+        rb.AddTorque(appliedTorque);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) //Use Collision2D for detect object
+    {
+        if (col.CompareTag("Zombie")) //if detect 'zombie'
+        {
+            Destroy(gameObject); 
+        }
+        else if (col.CompareTag("Wall")) //if detect 'wall'
+        {
+            Destroy(gameObject);
+        }
+    }
+}
